4645e91ff69488689857b8d66805213488e044ea
Allen Taylor <allen.taylor@canadensys.com>
2022-08-15 11:39:28 -0400

    Added standalone application.

---
+++ standalone/functions-print.cpp
@@ -0,0 +1,63 @@
+//
+// Standalone Asp application functions implementation: print.
+//
+
+#include "asp.h"
+#include <stdio.h>
+#include <limits.h>
+
+extern "C" AspRunResult asp_print
+    (AspEngine *engine,
+     AspDataEntry *value,
+     AspDataEntry **returnValue)
+{
+    if (AspIsNone(value))
+        printf("None");
+    if (AspIsEllipsis(value))
+        printf("...");
+    if (AspIsBoolean(value))
+        printf("%s", AspIsTrue(engine, value) ? "True" : "False");
+    else if (AspIsInteger(value))
+    {
+        int32_t i;
+        AspIntegerValue(value, &i);
+        printf("%d", i);
+    }
+    else if (AspIsFloat(value))
+    {
+        double f;
+        AspFloatValue(value, &f);
+        printf("%g", f);
+    }
+    else if (AspIsRange(value))
+    {
+        int32_t start, end, step;
+        AspRangeValue(engine, value, &start, &end, &step);
+        if (start != 0)
+            printf("%d", start);
+        printf("..");
+        bool unbounded =
+            step < 0 && end == INT32_MIN ||
+            step > 0 && end == INT32_MAX;
+        if (!unbounded)
+            printf("%d", end);
+        if (step != 1)
+            printf(":%d", step);
+    }
+    else if (AspIsString(value))
+    {
+        size_t size;
+        AspStringValue(engine, value, &size, 0, 0, 0);
+        char buffer[16];
+        for (size_t index = 0; index < size; index += sizeof buffer)
+        {
+            AspStringValue(engine, value, 0, buffer, index, sizeof buffer);
+            printf("%.*s", (int)(sizeof buffer), buffer);
+        }
+    }
+    else
+        printf("<unsupported type>");
+    puts("");
+
+    return AspRunResult_OK;
+}
---
+++ standalone/main.cpp
@@ -0,0 +1,99 @@
+//
+// Standalone Asp application main.
+//
+
+#include "asp.h"
+#include "standalone.h"
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+const size_t ASP_DATA_ENTRY_COUNT = 2048;
+const size_t ASP_CODE_BYTE_COUNT = 4096;
+
+int main(int argc, char **argv)
+{
+    // Obtain executable file name.
+    if (argc != 2)
+    {
+        cerr << "Specify program" << endl;
+        return 1;
+    }
+
+    // Open the executable file.
+    const char *executableFileName = argv[1];
+    FILE *fp = fopen(executableFileName, "rb");
+    if (fp == 0)
+    {
+        cerr << "Error opening " << executableFileName << endl;
+        return 1;
+    }
+
+    // Determine byte size of data area.
+    size_t dataEntrySize = AspDataEntrySize();
+    size_t dataByteSize = ASP_DATA_ENTRY_COUNT * dataEntrySize;
+
+    // Initialize the Asp engine.
+    AspEngine engine;
+    char *code = (char *)malloc(ASP_CODE_BYTE_COUNT);
+    char *data = (char *)malloc(dataByteSize);
+    AspRunResult initializeResult = AspInitialize
+        (&engine,
+         code, ASP_CODE_BYTE_COUNT,
+         data, dataByteSize,
+         &AspAppSpec_standalone);
+    if (initializeResult != AspRunResult_OK)
+    {
+        cerr
+            << "Error 0x" << hex << initializeResult
+            << " initializing Asp engine" << endl;
+        return 2;
+    }
+
+    // Load the executable.
+    while (true)
+    {
+        char c = fgetc(fp);
+        if (feof(fp))
+            break;
+        AspAddCodeResult addResult = AspAddCode(&engine, &c, 1);
+        if (addResult != AspAddCodeResult_OK)
+        {
+            cerr
+                << "Error 0x" << hex << addResult
+                << " loading code" << endl;
+            return 2;
+        }
+    }
+    AspAddCodeResult sealResult = AspSeal(&engine);
+    if (sealResult != AspAddCodeResult_OK)
+    {
+        cerr
+            << "Error 0x" << hex << sealResult
+            << " sealing code" << endl;
+        return 2;
+    }
+
+    // Run the code.
+    AspRunResult runResult;
+    while (true)
+    {
+        runResult = AspStep(&engine);
+        if (runResult != AspRunResult_OK)
+            break;
+    }
+
+    // Check completion status of the run.
+    if (runResult != AspRunResult_Complete)
+        cerr << "Run error 0x" << hex << runResult << endl;
+
+    // Dump data area in debug mode.
+    #ifdef ASP_DEBUG
+    cout << "Dump:" << endl;
+    AspDump(&engine, stdout);
+    #endif
+
+    return 0;
+}
