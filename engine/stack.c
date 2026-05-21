b8b508f28b1edd17e32314cd2a7013493b4da9cf
Allen B. Taylor <a.b.taylor@gmail.com>
2026-05-21 12:00:51 -0400

    Fixed an assert in the stack implementation.

--- engine/stack.c
+++ engine/stack.c
@@ -21,9 +21,8 @@ AspDataEntry *AspPushNoUse(AspEngine *engine, const AspDataEntry *value)
 
 static AspDataEntry *Push(AspEngine *engine, AspDataEntry *value, bool use)
 {
-    AspAssert(engine, value != 0);
     AspRunResult assertResult = AspAssert
-        (engine, AspDataGetType(value) != DataType_Free);
+        (engine, value != 0 && AspDataGetType(value) != DataType_Free);
     if (assertResult != AspRunResult_OK)
         return 0;
 
